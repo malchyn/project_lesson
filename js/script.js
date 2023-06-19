3407b7533d071b42a8ca631bfb2ee201c248367f
Danil <danila.striy@gmail.com>
2023-06-19 13:04:43 +0300

    third

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 "use strict";
+// data types
+/*
+
 console.log("Hello World!!!");
 const obj = { // obj 
     Name: "John",
@@ -19,6 +22,8 @@ fruits1.push("Pear"); // create in last element
 fruits1.shift(); // delate first element
 fruits1.unshift("PineApple"); // create first element
 */
+// if and others*
+/*
 
 let x = 4;
 if (5 == x) {
@@ -28,3 +33,86 @@ if (5 == x) {
 } else {
     console.log("No, it's not right!");
 } 
+(x == 50) ? console.log("Okay, it's right!") : console.log("No, it's not right!");
+
+
+const num = '100';
+const num_1 = 0;
+switch (num) {
+    case '100':
+        console.log("Yes");
+        break;
+    case 50:
+        console.log("Maybe");
+        break;
+    case 5:
+        console.log("No");
+        break;
+    default:
+        console.log("Never!!!");
+        break;
+}
+*/
+// some task
+/*
+
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger || cola || fries === 3 || nuggets) {
+    console.log('Done!');
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!');
+}
+*/
+// while and other
+/*
+let x = -1;
+
+while (x < 55) {
+    console.log("it's Right!");
+    x++;
+}
+
+do {
+    console.log("it's Right!");
+    x++;
+}
+while (x < 55);
+*/
+for (let i = 0; i < 10; i++) {
+    if (i == 0) {
+        console.log(i);
+    } else {
+        console.log("What?");
+    }
+}
